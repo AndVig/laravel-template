62682ba4f4c68a24bbdea12239dccde29e514875
AndVig <andrea.vigna2@gmail.com>
2024-07-14 17:41:37 +0200

    add header and footer

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+@include('shared.header')
+@include('shared.footer')
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Template</title>
+    @vite('resources/js/app.js')
+</head>
+<body>
+    <img src="{{ Vite::asset('resources/img/*')}}" alt="">
+
+    <main>
+        @yield('main')
+    </main>
+</body>
+</html>
